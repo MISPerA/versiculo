b5a49ff9960d54ade4fd43c767b059d88ba63242
MISPerA <143564926+MISPerA@users.noreply.github.com>
2023-09-09 17:45:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 // Lista de versículos bíblicos (substitua com seus próprios versículos)
 const versiculos = [
     {
+        versao: "Almeida Revista e Corrigida",
         livro: "João",
         capitulo: 3,
         versiculo: 16,
-        traducao: "Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna."
+        texto: "Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna."
     },
     {
+        versao: "NVI",
         livro: "Salmo",
         capitulo: 23,
         versiculo: 1,
-        traducao: "O Senhor é o meu pastor; nada me faltará."
+        texto: "O Senhor é o meu pastor; nada me faltará."
     },
     {
+        versao: "Almeida Revista e Corrigida",
         livro: "Filipenses",
         capitulo: 4,
         versiculo: 13,
-        traducao: "Tudo posso naquele que me fortalece."
+        texto: "Tudo posso naquele que me fortalece."
     },
     // Adicione mais versículos aqui
 ];
 
 const verseElement = document.getElementById('verse');
-const translationElement = document.querySelector('.translation');
+const versionElement = document.querySelector('.version');
 const bookChapterElement = document.querySelector('.book-chapter');
 
 // Função para gerar um versículo aleatório
@@ -30,9 +33,9 @@ function generateRandomVerse() {
     const randomIndex = Math.floor(Math.random() * versiculos.length);
     const randomVerse = versiculos[randomIndex];
 
-    // Atualize o conteúdo do versículo, tradução, livro e capítulo
-    verseElement.textContent = randomVerse.traducao;
-    translationElement.textContent = `Tradução: ${randomVerse.traducao}`;
+    // Atualize o conteúdo do versículo, versão, livro e capítulo
+    verseElement.textContent = randomVerse.texto;
+    versionElement.textContent = `Versão: ${randomVerse.versao}`;
     bookChapterElement.textContent = `Livro: ${randomVerse.livro}, Capítulo: ${randomVerse.capitulo}`;
 }
 
